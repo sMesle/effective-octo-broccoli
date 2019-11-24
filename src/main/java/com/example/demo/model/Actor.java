d04c2a9834e86b98d943fa53f1abc68b877ad19b
Steve Demeulemeester <steve.demeulemeester@efrei.net>
2019-11-25 00:28:32 +0100

    add actor model

---
+++ src/main/java/com/example/demo/model/Actor.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+public class Actor {
+
+    private String firstname;
+    private String lastname;
+
+    public Actor(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
