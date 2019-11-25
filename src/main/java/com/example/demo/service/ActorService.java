55a59593e396957b49131952abd9d261f5a9bc82
Steve Demeulemeester <steve.demeulemeester@efrei.net>
2019-11-25 14:31:51 +0100

    remove unused import

--- src/main/java/com/example/demo/service/ActorService.java
+++ src/main/java/com/example/demo/service/ActorService.java
@@ -4,8 +4,6 @@ import com.example.demo.model.Actor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.List;
 
 @Service
